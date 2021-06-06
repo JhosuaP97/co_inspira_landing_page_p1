bd6ea88d5d5a2fabdcdcff063325c1b37d96daf8
Juan Rojas <davidr2998@gmail.com>
2021-06-05 19:44:15 -0500

    WIP on bootcamp card

---
+++ src/Components/BootCard/card.styled.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const CardWrapper = styled.div`
+  width: 280px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
+  .o_card_image {
+    width: 100%;
+  }
+  .o_card_body {
+    display: flex;
+    flex-direction: column;
+    padding: 20px 10px 50px 10px;
+    text-align: justify;
+  }
+`;
